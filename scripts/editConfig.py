b8226c5b44489d5f60db4b1b32417f2de7e6dc0d
Larry Nguyen <larry.nguyen@branchapp.com>
2022-10-13 17:00:23 -0500

    test python

--- scripts/editConfig.py
+++ scripts/editConfig.py
@@ -1,6 +1,7 @@
 import yaml
 
-with open("../build-config.yml") as f:
-    y = yaml.safe_load(f)
-    y['workflows']['test']['envs']['MONTHLY_BUILD_NUMBER'] = '10'
-    print(yaml.dump(y, default_flow_style=False, sort_keys=False))
+print("hello world")
+# with open("../build-config.yml") as f:
+#     y = yaml.safe_load(f)
+#     y['workflows']['test']['envs']['MONTHLY_BUILD_NUMBER'] = '10'
+#     print(yaml.dump(y, default_flow_style=False, sort_keys=False))
