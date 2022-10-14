395d617f56d6544221f4c6a877cb47085659d15b
Larry Nguyen <larry.t.nguyen-1@outlook.com>
2022-10-14 09:38:32 -0500

    wip

--- scripts/editConfig.py
+++ scripts/editConfig.py
@@ -6,6 +6,6 @@ print("Writing to yml...")
 yaml = ruamel.yaml.YAML()
 
 with open("build-config.yml") as f:
-    y = yaml.safe_load(f)
+    y = yaml.load(f)
     y['workflows'][sys.argv[1]]['envs'][0]['MONTHLY_BUILD_NUMBER'] = sys.argv[2]
-    print(ruamel.yaml.dump(y))
+    print(yaml.dump(y))
