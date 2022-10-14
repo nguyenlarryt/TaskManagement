1cc2bf48026573252b92e2790013e5c69ce6454c
Larry Nguyen <larry.t.nguyen-1@outlook.com>
2022-10-14 09:15:09 -0500

    remove options

--- scripts/editConfig.py
+++ scripts/editConfig.py
@@ -5,4 +5,4 @@ print("Writing to yml...")
 with open("build-config.yml") as f:
     y = ruamel.yaml.load(f, Loader=ruamel.yaml.RoundTripLoader)
     y['workflows'][sys.argv[1]]['envs'][0]['MONTHLY_BUILD_NUMBER'] = sys.argv[2]
-    print(ruamel.yaml.dump(y, default_flow_style=False, sort_keys=False, Loader=ruamel.yaml.RoundTripLoader))
+    print(ruamel.yaml.dump(y, Loader=ruamel.yaml.RoundTripLoader))
