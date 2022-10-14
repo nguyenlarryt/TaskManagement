25f1bc9ba5a948c98a9b7adb3c7984fc2fc26c62
Larry Nguyen <larry.t.nguyen-1@outlook.com>
2022-10-14 07:03:01 -0500

    fix

--- scripts/editConfig.py
+++ scripts/editConfig.py
@@ -4,5 +4,5 @@ import yaml
 print("Writing to yml...")
 with open("build-config.yml") as f:
     y = yaml.safe_load(f)
-    y['workflows'][sys.argv[1]]['envs']['MONTHLY_BUILD_NUMBER'] = sys.argv[2]
+    y['workflows'][sys.argv[1]]['envs'][0]['MONTHLY_BUILD_NUMBER'] = sys.argv[2]
     print(yaml.dump(y, default_flow_style=False, sort_keys=False))
