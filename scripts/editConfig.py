39d075e1f638332dad2d6dfd93415ae9c3aaef64
Larry Nguyen <larry.t.nguyen-1@outlook.com>
2022-10-14 09:08:16 -0500

    change import

--- scripts/editConfig.py
+++ scripts/editConfig.py
@@ -3,6 +3,6 @@ import ruamel.yaml
 
 print("Writing to yml...")
 with open("build-config.yml") as f:
-    y = yaml.safe_load(f)
+    y = ruamel.yaml.safe_load(f, Loader=ruamel.yaml.RoundTripLoader)
     y['workflows'][sys.argv[1]]['envs'][0]['MONTHLY_BUILD_NUMBER'] = sys.argv[2]
-    print(yaml.dump(y, default_flow_style=False, sort_keys=False))
+    print(ruamel.yaml.dump(y, default_flow_style=False, sort_keys=False, Loader=ruamel.yaml.RoundTripLoader))
