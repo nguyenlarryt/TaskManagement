b1e74cf222217f60ee5ff2a83163fdc6aa59d01e
Larry Nguyen <larry.t.nguyen-1@outlook.com>
2022-10-14 09:19:17 -0500

    new api

--- scripts/editConfig.py
+++ scripts/editConfig.py
@@ -3,6 +3,6 @@ import ruamel.yaml
 
 print("Writing to yml...")
 with open("build-config.yml") as f:
-    y = ruamel.yaml.load(f, Loader=ruamel.yaml.RoundTripLoader)
+    y = ruamel.yaml.load(f)
     y['workflows'][sys.argv[1]]['envs'][0]['MONTHLY_BUILD_NUMBER'] = sys.argv[2]
-    print(ruamel.yaml.dump(y, Loader=ruamel.yaml.RoundTripLoader))
+    print(ruamel.yaml.dump(y))
