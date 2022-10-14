aa17f1ca9665bb37ca6d795e8b16807cc6b75061
Larry Nguyen <larry.t.nguyen-1@outlook.com>
2022-10-14 06:33:24 -0500

    update script

--- scripts/editConfig.py
+++ scripts/editConfig.py
@@ -1,7 +1,7 @@
 import yaml
 
-print("hello world")
-# with open("../build-config.yml") as f:
-#     y = yaml.safe_load(f)
-#     y['workflows']['test']['envs']['MONTHLY_BUILD_NUMBER'] = '10'
-#     print(yaml.dump(y, default_flow_style=False, sort_keys=False))
+print("Writing to yml...")
+with open("../build-config.yml") as f:
+    y = yaml.safe_load(f)
+    y['workflows'][sys.argv[1]]['envs']['MONTHLY_BUILD_NUMBER'] = sys.arv[2]
+    print(yaml.dump(y, default_flow_style=False, sort_keys=False))
